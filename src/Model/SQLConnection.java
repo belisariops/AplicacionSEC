3f6dc69a535313505fde14678d0e75ba98aea798
belisariops <belisariops@gmail.com>
2017-01-23 17:59:49 -0300

    Probando FX

---
+++ src/Model/SQLConnection.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.sql.*;
+/**
+ * Created by bpanay on 23-01-2017.
+ */
+public class SQLConnection {
+    SQLConnection connection;
+
+    public SQLConnection() {
+        connection = new SQLConnection()
+    }
+}
