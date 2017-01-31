dc3c7b89920103d7eb7f826f2e71f2ca00fe8744
juakotorres <juako.torres@hotmail.com>
2017-01-31 11:38:59 -0300

    listado tablas y barra de progreso

--- src/Model/Tabla.java
+++ src/Model/Tabla.java
@@ -3,10 +3,10 @@ package Model;
 /**
  * Created by juakotorres on 27-01-2017.
  */
-public class Table {
+public class Tabla {
     private String name;
 
-    public Table(String name){
+    public Tabla(String name){
         this.name = name;
     }
 
